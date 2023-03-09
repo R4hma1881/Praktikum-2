29db2dad3ae6a5a8de935c2cf8011fe9ffc38569
R4hma1881 <rahmadiannurhikma0>
2023-03-08 17:31:52 -1100

    prektikum 2

---
+++ kondisional_switchcase.php
@@ -0,0 +1,18 @@
+<?php
+$angka = 10;
+
+switch($angka){
+    case 9 :
+        echo "benar, ini 9 !";
+        break;
+    case 8 : 
+        echo "benar, ini 8 !";
+        break;
+    case 10 :
+        echo "benar, ini 10 !";
+        break;
+    default :
+    echo "ya ini berapa aja bisa";
+    break;
+
+}
